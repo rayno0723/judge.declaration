426420baf2d0b5e399ddb810711ee1d3a276c44b
rayno0723 <61087487+rayno0723@users.noreply.github.com>
2026-09-18 16:43:01 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const CACHE="judge-script-cache-v1";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"]))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}))));
